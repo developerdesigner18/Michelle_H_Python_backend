0b60e2e00c50c06808c0fb483547718a3a16e85b
DDSuser3 <kishankalariya.dds@gmail.com>
2023-07-05 12:52:52 +0530

    change URLs.py in core for users

--- core/urls.py
+++ core/urls.py
@@ -28,7 +28,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path('users/',include('Users_app.urls')),
+    path('api/users/',include('Users_app.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
